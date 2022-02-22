284b07d2a1f2dfe985c2f4ca78818b0dac9a1a5c
pjw9412 <pjwts9412@gmail.com>
2022-02-22 18:35:39 +0900

    trying to modularity

---
+++ src/main.js
@@ -0,0 +1,14 @@
+'ust strict';
+
+import Buttons from './buttons';
+
+const mapOptions = {
+    center: new naver.maps.LatLng(37.55916069282345, 126.8410640925702), // 지도의 초기 중심 좌표(위도와 경도)
+    zoom: 18, // 지도의 초기 줌 레벨
+    zoomControl: true, // 줌 컨트롤 표시 여부
+    zoomControlOptions: {
+        position: naver.maps.Position.TOP_RIGHT,
+    },
+};
+
+const map = new naver.maps.Map('map', mapOptions);
