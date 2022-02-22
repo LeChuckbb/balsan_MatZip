f38ff12b16eab2b0f0739adcd31b2226ed45b843
pjw9412 <pjwts9412@gmail.com>
2022-02-22 19:07:01 +0900

    add custom button positioned to ELCRU when clicked'

--- src/buttons.js
+++ src/buttons.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const locationBtnHtml = `<a href="#" class="btn_mylct">
+                                <span class="spr_trff spr_ico_mylct">발산역 청년주택</span>
+                          </a>`;
+
 export default class Buttons {
-    constructor() {}
+    constructor(map) {
+        this.map = map;
+    }
 }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@
 
 import Buttons from './buttons.js';
 
+const LATITUDE = 37.55916069282345;
+const LONGITUDE = 126.8410640925702;
+
 const mapOptions = {
-    center: new naver.maps.LatLng(37.55916069282345, 126.8410640925702), // 지도의 초기 중심 좌표(위도와 경도)
+    center: new naver.maps.LatLng(LATITUDE, LONGITUDE), // 지도의 초기 중심 좌표(위도와 경도)
     zoom: 18, // 지도의 초기 줌 레벨
     zoomControl: true, // 줌 컨트롤 표시 여부
     zoomControlOptions: {
@@ -12,3 +15,20 @@ const mapOptions = {
 };
 
 const map = new naver.maps.Map('map', mapOptions);
+const btns = new Buttons(map);
+
+const locationBtnHtml = `<a href="#" class="btn_mylct">
+                                <span class="spr_trff spr_ico_mylct">발산역 청년주택</span>
+                        </a>`;
+
+naver.maps.Event.once(map, 'init', function () {
+    const customControl = new naver.maps.CustomControl(locationBtnHtml, {
+        position: naver.maps.Position.TOP_LEFT,
+    });
+
+    customControl.setMap(map); // map에 보이게 하는 것?
+
+    naver.maps.Event.addDOMListener(customControl.getElement(), 'click', function () {
+        map.setCenter(new naver.maps.LatLng(LATITUDE, LONGITUDE));
+    });
+});
