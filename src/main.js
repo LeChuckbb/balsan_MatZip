7163887b311eb7ec51ba57789881ae34647dc155
pjw9412 <pjwts9412@gmail.com>
2022-02-22 19:20:27 +0900

    add HOME icon

--- src/main.js
+++ src/main.js
@@ -17,6 +17,8 @@ const mapOptions = {
 const map = new naver.maps.Map('map', mapOptions);
 const btns = new Buttons(map);
 
+/* CustomControl 객체를 이용한 '발산역 엘크루' 버튼 추가하기 */
+// exam ) 사용자 정의 컨트롤 만들기
 const locationBtnHtml = `<a href="#" class="btn_mylct">
                                 <span class="spr_trff spr_ico_mylct">발산역 청년주택</span>
                         </a>`;
@@ -32,3 +34,21 @@ naver.maps.Event.once(map, 'init', function () {
         map.setCenter(new naver.maps.LatLng(LATITUDE, LONGITUDE));
     });
 });
+
+/* HOME Marker 추가하기 */
+// exam) 이미지 아이콘 사용하기
+const HOME_PATH = window.HOME_PATH || '.';
+
+const markerOptions = {
+    position: position.destinationPoint(90, 15),
+    map: map,
+    icon: {
+        url: `${HOME_PATH}/img/home.png`,
+        size: new naver.maps.Size(50, 52),
+        origin: new naver.maps.Point(0, 0),
+        anchor: new naver.maps.Point(25, 26),
+    },
+};
+
+const marker = new naver.maps.Marker(markerOptions);
+console.log(marker);
